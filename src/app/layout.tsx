8c5cac702477e3fbfea8d7b4883bcf46925a5e71
Tim Dean <timdeangordo@gmail.com>
2026-07-05 12:25:24 +0000

    eu cheguei a te enviar o codigo do meta com esse pixel nele? 23322448906

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className="font-body antialiased font-light">
+        {/* LowTrack Pixel */}
         <Script id="lowtrack-pixel" strategy="afterInteractive">
           {`
             window.pixelId = "lt_px_c125cbd8ac6d";
@@ -52,6 +53,32 @@ export default function RootLayout({
             document.head.appendChild(a);
           `}
         </Script>
+
+        {/* Meta Pixel */}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2332244890653778');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2332244890653778&ev=PageView&noscript=1"
+            alt="facebook pixel"
+          />
+        </noscript>
+
         {children}
         <Toaster />
       </body>
